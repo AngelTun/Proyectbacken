43a822a31cff5f2446a5c02e2ced4f16a61a34f4
AngelTun <159564191+AngelTun@users.noreply.github.com>
2024-11-07 09:02:24 -0600

    3er cambio

--- apps/views.py
+++ apps/views.py
@@ -9,5 +9,21 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'inicio.html')
 
-def inicio(request):
-    return render(request, 'catalogo.html')
+
+def registro(request):
+    
+    if request.method == 'GET':
+          return render(request, 'registro.html', {
+        'form': UserCreationForm 
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registro de usuarion
+            try:
+                user = User.objects.create_user(username=request.POST['username'], 
+                                     password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado correctamente')
+            except:
+                return HttpResponse('El usuario ya existe')
+        return HttpResponse('Las contraseñas no coinciden' )
